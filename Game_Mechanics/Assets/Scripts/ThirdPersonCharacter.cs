8490b357cdb037697b66e4cb2a03691f6db05a5e
Casey Kusella-Gussin <ckgussindev@googlemail.com>
2024-06-15 18:22:06 +0100

    Movement
    -Movement is added to the player

--- Game_Mechanics/Assets/Scripts/ThirdPersonCharacter.cs
+++ Game_Mechanics/Assets/Scripts/ThirdPersonCharacter.cs
@@ -1,37 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(CharacterController))]
 
 public class ThirdPersonCharacter : MonoBehaviour
 {
     private float movementSpeed = 4.0f;
+    private CharacterController cc;
+    private Vector2 characterMovement;
     
     // Start is called before the first frame update
     void Start()
     {
         
+        Debug.Log("Player created");
+        cc = GetComponent<CharacterController>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+        
+    }
 
-        if (Input.GetButton("Horizontal"))
-        {
-            
-            transform.Translate();
-            
-            
-        }
-        if (Input.GetButton("Vertical"))
-        {
-            
-            
-            
-            
-        }
+    void MovePlayer()
+    {
 
+        Vector3 movement = new Vector3(characterMovement.x, 0.0f, characterMovement.y);
+        cc.SimpleMove(movement * movementSpeed);
 
+    }
+    
+    
+    void OnMove(InputValue iv)
+    {
+
+        Debug.Log("Movement pressed");
+        characterMovement = iv.Get<Vector2>();
 
     }
 }
