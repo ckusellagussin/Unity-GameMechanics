1882e53a6cdd74ab59593b73bd254d7958209d63
Casey Kusella-Gussin <ckgussindev@googlemail.com>
2024-10-06 22:59:18 +0100

    Clamping (Not working)

--- Game_Mechanics/Assets/Scripts/RotateObject/RotateObject.cs
+++ Game_Mechanics/Assets/Scripts/RotateObject/RotateObject.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
@@ -10,11 +11,11 @@ public class RotateObject : MonoBehaviour
 
     
     public float speed = 10f;
-    public float pitch = 0f;
     private bool isRotating = false;
     public float mouseMovement;
     public float currentMousePosition;
     private float startMousePosition;
+    private float pitch;
     
     
     // Start is called before the first frame update
@@ -29,7 +30,8 @@ public class RotateObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        
+     
         if (Input.GetMouseButtonDown(1))
         {
 
@@ -45,23 +47,25 @@ public class RotateObject : MonoBehaviour
 
         }
 
-        
-        
+
+
         if (isRotating)
         {
-            currentMousePosition = Input.mousePosition.y;
-            mouseMovement = currentMousePosition - startMousePosition;
-            
-            transform.Rotate(Vector3.right, -mouseMovement * speed * Time.deltaTime);
-        
-            startMousePosition = currentMousePosition;
-            
-            transform.localRotation = Mathf.Clamp(currentMousePosition, -30, 30);
+                
+                currentMousePosition = Input.mousePosition.y;
+                mouseMovement = currentMousePosition - startMousePosition;
+                
 
+                transform.Rotate(Vector3.right, -mouseMovement * speed * Time.deltaTime);
+                
+                startMousePosition = currentMousePosition;
+
+
+                pitch = Mathf.Clamp(mouseMovement, -3000f, 3000f);
+                transform.localRotation = quaternion.Euler(pitch);
+             
         }
 
         
-
-
     }
 }
