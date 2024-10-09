e2a6d7a5c12966c8ace49f221c22898a30d4f802
Casey Kusella-Gussin <ckgussindev@googlemail.com>
2024-10-09 21:50:50 +0100

    Arrow sticks into ground (Broken)

--- Game_Mechanics/Assets/Scripts/BowMechanic/ArrowProperties.cs
+++ Game_Mechanics/Assets/Scripts/BowMechanic/ArrowProperties.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 
 public class ArrowProperties : MonoBehaviour
@@ -7,12 +9,43 @@ public class ArrowProperties : MonoBehaviour
     public Rigidbody Arrow;
     public float angularSpeed = 2.0f;
     public float torque = 2f;
+    public float arrowTipOffset = -5;
+    
+    private bool isStuck = false;
+    
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
+        Arrow = GetComponent<Rigidbody>();
+       
     }
+
+    private void OnCollisionEnter(Collision col)
+    { 
+        if (!isStuck)
+        {
+
+            if (col.gameObject.CompareTag("GroundX"))
+            {
+                Arrow.velocity = Vector3.zero;
+                Arrow.angularVelocity = Vector3.zero;
+                Arrow.isKinematic = true;
+                
+                Vector3 hitNormal = col.contacts[0].normal;
+                transform.rotation = Quaternion.LookRotation(-hitNormal);
+
+                Vector3 arrowPosition = col.contacts[0].point - transform.forward * arrowTipOffset;
+                transform.position = arrowPosition;
+
+                isStuck = true;
+                
+                Debug.Log(isStuck);
+            }
+
+        }
+}
 
     // Update is called once per frame
     void Update()
